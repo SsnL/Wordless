228dfe38076f37f815538bce1f02ed926e6eb07d
hr_zhu <hr_zhu@airbears2-10-142-3-4.airbears2.1918.berkeley.edu>
2014-10-04 13:08:50 -0700

    message class

---
+++ Wordless/RSTMessage.swift
@@ -0,0 +1,15 @@
+//
+//  RSTMessage.swift
+//  Wordless
+//
+//  Created by hr_zhu on 14-10-4.
+//  Copyright (c) 2014年 RST. All rights reserved.
+//
+
+import Foundation
+class RSTMessage {
+    var sender: PFUser?
+    var receiver: PFUser?
+    var content: String?
+//    int sentiment
+}
