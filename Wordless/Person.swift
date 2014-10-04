b6c929c02eafc18b61130ce616a78a489094f6cb
SsnL <tongzhou.wang.1994@gmail.com>
2014-10-03 21:05:40 -0700

    add person

---
+++ Wordless/Person.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Wordless
+//
+//  Created by S_sn on 10/3/14.
+//  Copyright (c) 2014 RST. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    
+    var name : String;
+    
+    init(name n: String) {
+        name = n;
+    }
+}
